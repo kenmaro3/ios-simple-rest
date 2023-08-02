3ab9188e7581b2f4a05044a78620f87b9dec192c
kenmaro3 <miha.ken.19@gmail.com>
2023-08-02 18:01:58 +0900

    Initial Commit

---
+++ SimpleRest/SimpleRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRestApp.swift
+//  SimpleRest
+//
+//  Created by Kentaro Mihara on 2023/08/02.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
